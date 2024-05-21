331c99ae09fc47e45678b781c17710c5f1992c16
jakariamasum <jmphy698@gmail.com>
2024-05-21 23:26:53 +0600

    product insert to db

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { Application, Request, Response } from "express";
+import { productRoutes } from "./modules/product/product.route";
+import cors from 'cors'
 const app: Application = express();
 const port = 3000;
+
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+app.use('/api',productRoutes)
 export default app;
---
+++ src/modules/product/product.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { productServices } from "./product.service";
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const product = req.body;
+    const result = await productServices.createProductToDB(product);
+    res.status(200).json({
+      success: true,
+      message: "Product created successfully!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      data: error,
+    });
+  }
+};
+
+
+export const productControllers={
+    createProduct,
+}
